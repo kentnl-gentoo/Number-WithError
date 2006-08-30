383c1a81b2550490772997822df776fa6436ec94
SMUELLER <smueller@cpan.org>
2006-08-30 11:19:40 +0000

    Import https://cpan.metacpan.org/authors/id/S/SM/SMUELLER/Number-WithError-0.06.tar.gz

--- lib/Number/WithError.pm
+++ lib/Number/WithError.pm
@@ -6,7 +6,7 @@ use warnings;
 use Params::Util qw/_ARRAY _INSTANCE _ARRAY0/;
 use prefork 'Math::BigFloat';
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use base 'Exporter';
 our @EXPORT_OK = qw(
